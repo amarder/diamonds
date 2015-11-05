f68137403f90a0c14ef551a5d6ccfcbd44ba7c20
Andrew Marder <andrew.n.marder@gmail.com>
2015-11-05 09:18:22 -0500

    Got download script working smoothly. Need to add command line support next.

--- download.py
+++ download.py
@@ -1,15 +1,71 @@
-# http://www.bluenile.com/api/public/build-your-own-ring/diamond-search-grid?country=USA&language=en-us&currency=USD&startIndex=0&pageSize=250000&shape=RD
-
 import json
 import pandas as pd
 import re
-from matplotlib import pyplot as plt
+import requests
 
-with open('diamonds.json') as f:
-    d = json.load(f)
 
-df = pd.DataFrame(d['results'])
-df['price'] = df.price.map(lambda s: re.sub('[$,]', '', s)).astype(int)
-df['carat'] = df.carat.astype(float)
+def download(params):
+    # Get the data from Blue Nile.
+    # http://stackoverflow.com/questions/26165656/rcurl-r-package-geturl-webpage-error-when-scraping-api
+    landing_page = requests.get('http://www.bluenile.com/')
+    url = 'http://www.bluenile.com/api/public/diamond-search-grid/solr'
+    response = requests.get(url, params, cookies=landing_page.cookies)
 
-df.to_csv('diamonds.csv')
+    # Put the data into a data frame.
+    d = json.loads(response.text)
+    df = pd.DataFrame(d['results'])
+
+    # Clean up the data.
+    for col in ['carat', 'depth', 'lxwRatio', 'table']:
+        df[col] = df[col].astype(float)
+    for col in ['price', 'pricePerCarat']:
+        df[col] = df[col].map(lambda s: re.sub('[$,]', '', s)).astype(int)
+
+    return df
+
+
+shapes = [
+        "AS", # asscher
+        "CU", # cushion
+        "EC", # emerald
+        "HS", # heart
+        "MQ", # marquise
+        "OV", # oval
+        "PR", # princess
+        "PS", # pear
+        "RA", # radiant
+        "RD", # round
+    ]
+cuts = ['Good', 'Very Good', 'Ideal', 'Signature Ideal']
+colors = ['J', 'I', 'H', 'G', 'F', 'E', 'D']
+clarities = ['SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF', 'FL']
+options = {
+    'shape': shapes,
+    'minPrice': int,
+    'maxPrice': int,
+    'minCarat': float,
+    'maxCarat': float, 
+    'minCut': cuts,
+    'maxCut': cuts,
+    'minColor': colors,
+    'maxColor': colors,
+    'minClarity': clarities,
+    'maxClarity': clarities,
+    'startIndex': int,
+    'pageSize': int,
+}
+
+
+if __name__ == '__main__':
+    params = {
+        'country': 'USA',
+        'language': 'en-us',
+        'currency': 'USD',
+        'startIndex': 0,
+        'pageSize': 10,
+        'shape': ['RD', 'CU'],
+    }
+
+    df = download(params)
+    print df
+    print df.dtypes
