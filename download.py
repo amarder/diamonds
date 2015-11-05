6cf8a78f90e6d7166775a5b375dec5d2f13f6810
Andrew Marder <andrew.n.marder@gmail.com>
2015-11-05 10:16:11 -0500

    Incorporated choice lists into parser.

--- download.py
+++ download.py
@@ -29,37 +29,37 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
 
     shapes = [
-        "RD"  # round
-        "PR"  # princess
-        "EC"  # emerald
-        "AS"  # asscher
-        "CU"  # cushion
-        "MQ"  # marquise
-        "RA"  # radiant
-        "OV"  # oval
-        "PS"  # pear
-        "HS"  # heart
+        "RD",  # round
+        "PR",  # princess
+        "EC",  # emerald
+        "AS",  # asscher
+        "CU",  # cushion
+        "MQ",  # marquise
+        "RA",  # radiant
+        "OV",  # oval
+        "PS",  # pear
+        "HS",  # heart
     ]
-    cuts = ['Good', 'Very Good', 'Ideal', 'Signature Ideal']
-    colors = ['J', 'I', 'H', 'G', 'F', 'E', 'D']
-    clarities = ['SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF', 'FL']
+    parser.add_argument('--shape', nargs='+', choices=shapes)
 
-    parser.add_argument('--shape', nargs='+')
     parser.add_argument('--minPrice', type=int)
     parser.add_argument('--maxPrice', type=int)
     parser.add_argument('--minCarat', type=float)
     parser.add_argument('--maxCarat', type=float)
 
+    cuts = ['Good', 'Very Good', 'Ideal', 'Signature Ideal']
+    colors = ['J', 'I', 'H', 'G', 'F', 'E', 'D']
+    clarities = ['SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF', 'FL']
     select_one = [
-        'minCut',
-        'maxCut',
-        'minColor',
-        'maxColor',
-        'minClarity',
-        'maxClarity',
+        ('minCut', cuts),
+        ('maxCut', cuts),
+        ('minColor', colors),
+        ('maxColor', colors),
+        ('minClarity', clarities),
+        ('maxClarity', clarities),
     ]
-    for col in select_one:
-        parser.add_argument('--%s' % col)
+    for key, choices in select_one:
+        parser.add_argument('--%s' % key, choices=choices)
 
     arguments_with_defaults = [
         ('startIndex', 0),
@@ -70,6 +70,7 @@ def parse_arguments():
     ]
     for k, v in arguments_with_defaults:
         parser.add_argument('--%s' % k, default=v, type=type(v))
+
     args = parser.parse_args()
     d = args.__dict__
     for k, v in d.items():
