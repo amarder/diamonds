d2be1f98ba1ebdc7e33907dab371cb8327e0e587
Andrew Marder <andrew.n.marder@gmail.com>
2015-11-05 10:07:55 -0500

    Set up command line argument parsing.

--- download.py
+++ download.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd
 import re
 import requests
+import argparse
 
 
 def download(params):
@@ -24,48 +25,64 @@ def download(params):
     return df
 
 
-shapes = [
-        "AS", # asscher
-        "CU", # cushion
-        "EC", # emerald
-        "HS", # heart
-        "MQ", # marquise
-        "OV", # oval
-        "PR", # princess
-        "PS", # pear
-        "RA", # radiant
-        "RD", # round
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    shapes = [
+        "RD"  # round
+        "PR"  # princess
+        "EC"  # emerald
+        "AS"  # asscher
+        "CU"  # cushion
+        "MQ"  # marquise
+        "RA"  # radiant
+        "OV"  # oval
+        "PS"  # pear
+        "HS"  # heart
     ]
-cuts = ['Good', 'Very Good', 'Ideal', 'Signature Ideal']
-colors = ['J', 'I', 'H', 'G', 'F', 'E', 'D']
-clarities = ['SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF', 'FL']
-options = {
-    'shape': shapes,
-    'minPrice': int,
-    'maxPrice': int,
-    'minCarat': float,
-    'maxCarat': float, 
-    'minCut': cuts,
-    'maxCut': cuts,
-    'minColor': colors,
-    'maxColor': colors,
-    'minClarity': clarities,
-    'maxClarity': clarities,
-    'startIndex': int,
-    'pageSize': int,
-}
+    cuts = ['Good', 'Very Good', 'Ideal', 'Signature Ideal']
+    colors = ['J', 'I', 'H', 'G', 'F', 'E', 'D']
+    clarities = ['SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF', 'FL']
+
+    parser.add_argument('--shape', nargs='+')
+    parser.add_argument('--minPrice', type=int)
+    parser.add_argument('--maxPrice', type=int)
+    parser.add_argument('--minCarat', type=float)
+    parser.add_argument('--maxCarat', type=float)
+
+    select_one = [
+        'minCut',
+        'maxCut',
+        'minColor',
+        'maxColor',
+        'minClarity',
+        'maxClarity',
+    ]
+    for col in select_one:
+        parser.add_argument('--%s' % col)
+
+    arguments_with_defaults = [
+        ('startIndex', 0),
+        ('pageSize', 10),
+        ('country', 'USA'),
+        ('language', 'en-us'),
+        ('currency', 'USD'),
+    ]
+    for k, v in arguments_with_defaults:
+        parser.add_argument('--%s' % k, default=v, type=type(v))
+    args = parser.parse_args()
+    d = args.__dict__
+    for k, v in d.items():
+        if v is None:
+            del d[k]
+    return d
+
+
+def main():
+    params = parse_arguments()
+    df = download(params)
+    print df.to_csv(index=False)
 
 
 if __name__ == '__main__':
-    params = {
-        'country': 'USA',
-        'language': 'en-us',
-        'currency': 'USD',
-        'startIndex': 0,
-        'pageSize': 10,
-        'shape': ['RD', 'CU'],
-    }
-
-    df = download(params)
-    print df
-    print df.dtypes
+    main()
